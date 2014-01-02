c2d2eadad103845a762f87433e0c3c2b44f8fbc4
llohellohe <yangqichris@gmail.com>
2014-01-02 23:37:04 +0800

    add post

---
+++ src/main/java/yangqi/zookeeper/example/masterworker/ChildrenCallbackMonitor.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright 1999-2010 Alibaba.com All right reserved. This software is the
+ * confidential and proprietary information of Alibaba.com ("Confidential
+ * Information"). You shall not disclose such Confidential Information and shall
+ * use it only in accordance with the terms of the license agreement you entered
+ * into with Alibaba.com.
+ */
+package yangqi.zookeeper.example.masterworker;
+
+import java.io.IOException;
+import java.util.List;
+
+import org.apache.zookeeper.AsyncCallback.ChildrenCallback;
+import org.apache.zookeeper.WatchedEvent;
+import org.apache.zookeeper.Watcher;
+import org.apache.zookeeper.Watcher.Event.EventType;
+import org.apache.zookeeper.ZooKeeper;
+
+public class ChildrenCallbackMonitor {
+
+    /**
+     * @param args
+     * @throws IOException
+     * @throws InterruptedException
+     */
+    public static void main(String[] args) throws IOException, InterruptedException {
+        final ZooKeeper zookeeper = new ZooKeeper("localhost:2181", 2000, null);
+        
+        final ChildrenCallback callback = new ChildrenCallback() {
+
+            @Override
+            public void processResult(int rc, String path, Object ctx, List<String> children) {
+                System.out.println(children);
+
+            }
+            
+        };
+        
+        Watcher watcher = new Watcher() {
+            @Override
+            public void process(WatchedEvent event) {
+                System.out.println("Event is " + event);
+                if (event.getType() == EventType.NodeChildrenChanged) {
+                    System.out.println("Changed " + event);
+                    zookeeper.getChildren("/workers", this, callback, null);
+                }
+            }
+        };
+        
+        zookeeper.getChildren("/workers", watcher, callback, null);
+
+
+        Thread.sleep(200000);
+
+    }
+
+}
