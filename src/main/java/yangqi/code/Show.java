11ca015bb9548f99d8d5bde0ab5e05c56d00ba7f
llohellohe <yangqichris@gmail.com>
2014-05-03 11:12:12 +0800

    增加show

---
+++ src/main/java/yangqi/code/Show.java
@@ -0,0 +1,15 @@
+package yangqi.code;
+
+/**
+ * Created by yangqi on 5/1/14.
+ */
+public class Show {
+    public boolean show(){
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Show show=new Show();
+        show.show();
+    }
+}
